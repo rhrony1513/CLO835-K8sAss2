812afa96ca8c5ed280afe7d37646fd8e319be2ab
rhrony1513 <rontertainmenttv@gmail.com>
2025-02-27 12:21:23 -0800

    Update init_kind.sh to create Kind cluster using kind.yaml

--- init_kind.sh
+++ init_kind.sh
@@ -22,3 +22,6 @@ rm -f ./kind
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 rm -f ./kubectl
+
+# Create Kind cluster
+kind create cluster --config kind.yaml
